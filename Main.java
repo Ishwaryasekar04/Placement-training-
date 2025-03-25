c5a87fd3cae3efc33216ee3a2919f86190eee455
Ishwaryasekar04 <ishwaryasekar50@gmail.com>
2025-03-25 14:10:46 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,10 +4,18 @@ public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 System.out.print("Enter number:");
 int n=sc.nextInt();
-if(n%2==0){
-System.out.print(n+ " is a even number");
-}else{
-System.out.print(n+ " is a odd number");
+int temp=n;
+int digit=0;
+int rev=0;
+while(temp>0){
+temp=temp/10;
+digit++;
 }
+for(int i=1;i<=digit;i++){
+int digits=n%10;
+rev=rev*10+digits;
+n=n/10;
+}
+System.out.print(rev);
 }
 }
